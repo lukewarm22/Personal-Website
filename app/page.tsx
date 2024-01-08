8431e3b4f1cb89f10c6d7287a8ac1cd4f1e44a58
Luke Jordan <lwjorda@clemson.edu>
2024-01-08 11:58:51 -0500

    fixed for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,16 @@ import Image from "next/image";
 import Link from "next/link";
 import personalPic from "../public/me.jpg";
 import Button from "@mui/material/Button";
+import { useState } from "react";
+
+
 
 export default function Home() {
-  return (
+  
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  window.addEventListener("resize", () => {setWindowWidth(window.innerWidth)});
+  const isMobile = windowWidth <= 500;
+  if(!isMobile) return (
     
     <div style={{ minHeight: "80%" }}>
       <img
@@ -75,7 +82,7 @@ export default function Home() {
           window.location.href = "https://random.dog";
         }}
       >
-        Picture Of Dog
+        Picture Of A Dog
       </Button>
       <br />
       <Button
@@ -112,4 +119,111 @@ export default function Home() {
       </Button>
     </div>
   );
+  else return(
+    <div style={{ minHeight: "80%" }}>
+      <img
+        src="/me.jpg"
+        alt="picture of me"
+        width={0}
+        height={0}
+        sizes="100vw"
+        style={{
+          display: "block",
+          width: 250,
+          height: 250,
+          marginLeft: "auto",
+          marginRight: "auto",
+          borderRadius: "100%",
+          marginTop: "5%",
+          borderColor: "black",
+          borderStyle: "solid",
+          borderWidth: 4,
+        }}
+      />
+      <h1
+        style={{
+          color: "peru",
+          fontSize: "4rem",
+          fontWeight: "bold",
+          textAlign: "center",
+          marginRight: "auto",
+          marginLeft: "auto",
+          fontFamily: "sans-serif",
+          marginBottom: "3%",
+          paddingRight: 0,
+          marginTop:2
+        }}
+      >
+      Luke Jordan
+      </h1>
+
+
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "burlywood",
+          width: "33%",
+          height: 200,
+          fontSize: "27px",
+          marginLeft: "5%",
+          marginTop: "2%",
+        }}
+        onClick={() => {
+          window.location.href = "/projects";
+        }}
+      >
+        Projects
+      </Button>
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "burlywood",
+          width: "33%",
+          height: 200,
+          fontSize: "27px",
+          marginLeft: "25%",
+          marginTop: "2%",
+        }}
+        onClick={() => {
+          window.location.href = "https://random.dog";
+        }}
+      >
+        Picture Of A Dog
+      </Button>
+      <br />
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "burlywood",
+          width: "33%",
+          height: 200,
+          fontSize: "27px",
+          marginLeft: "5%",
+          marginTop: "2%",
+        }}
+        onClick={() => {
+          window.location.href = "/projects";
+        }}
+      >
+        About Me
+      </Button>
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "burlywood",
+          width: "33%",
+          height: 200,
+          fontSize: "27px",
+          marginLeft: "25%",
+          marginTop: "2%",
+        }}
+        onClick={() => {
+          window.location.href = "/projects";
+        }}
+      >
+        Contact Me
+      </Button>
+    </div>
+  );
+  
 }
